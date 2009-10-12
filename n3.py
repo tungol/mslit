c671a12a458432662bc074475c5cae868b2e730f
Stephen <Stephen@enfield169-240.hampshire.edu>
2009-10-11 21:00:31 -0400

    zerocombine n3 attempt 1

--- n3.py
+++ n3.py
@@ -1,8 +1,35 @@
 #! /usr/bin/env python
 
-import sys
+import sys, os
 from pyraf import iraf
 
+PREFIX="cghn300"
+LOCATION="../n3/"
+
+def get_names(range):
+	if type(range) == int:
+		range = [range]
+	if type(range) == list:
+		tmp = []
+		for item in range:
+			if item < 10:
+				tmp.append(PREFIX+'0'+str(item))
+			else:
+				tmp.append(PREFIX+str(item))
+		return tmp
+	else:
+		raise TypeError("not a range")
+
+def combine_zeros(zeros):
+	iraf.noao(_doprint=0)
+	iraf.imred(_doprint=0)
+	iraf.ccdred(_doprint=0)
+	iraf.unlean("zerocombine")
+	iraf.zerocombine(zeros)
+
 def main():
-	zeros = get_names('cghn300', range(1,15))
-	combine_zeros
+	os.chdir(LOCATION)
+	zeros = get_names(range(1,15))
+	combine_zeros(zeros)
+
+main()
