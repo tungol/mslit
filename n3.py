efff2dc44df0d307bfcaafb7a349fba4092dcf59
Stephen <Stephen@enfield169-240.hampshire.edu>
2009-10-11 21:38:01 -0400

    working zerocombine, uses lists

--- n3.py
+++ n3.py
@@ -3,33 +3,17 @@
 import sys, os
 from pyraf import iraf
 
-PREFIX="cghn300"
 LOCATION="../n3/"
 
-def get_names(range):
-	if type(range) == int:
-		range = [range]
-	if type(range) == list:
-		tmp = []
-		for item in range:
-			if item < 10:
-				tmp.append(PREFIX+'0'+str(item))
-			else:
-				tmp.append(PREFIX+str(item))
-		return tmp
-	else:
-		raise TypeError("not a range")
-
-def combine_zeros(zeros):
+def combine_zeros():
 	iraf.noao(_doprint=0)
 	iraf.imred(_doprint=0)
 	iraf.ccdred(_doprint=0)
-	iraf.unlean("zerocombine")
-	iraf.zerocombine(zeros)
+	iraf.zerocombine.unlearn()
+	iraf.zerocombine(input="@lists/zero")
 
 def main():
 	os.chdir(LOCATION)
-	zeros = get_names(range(1,15))
-	combine_zeros(zeros)
+	combine_zeros()
 
 main()
