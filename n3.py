2ae8cddeebb3d250947977555aa3eff1c8789f40
Stephen <Stephen@enfield173-221.hampshire.edu>
2009-10-13 12:07:11 -0400

    first attempt at processing ngc4725

--- n3.py
+++ n3.py
@@ -17,13 +17,18 @@ def combine_flat2():
 	iraf.flatcombine.unlearn()
 	iraf.flatcombine(input="@lists/flat2", output="Flat2", process="no")
 
+def ccdproc_ngc4725():
+	iraf.ccdproc.unlearn()
+	iraf.ccdproc.(images="@lists/ngc4725", fixpix="no", darkcor="no", biassec="[2049:2080,1:501]", trimsec="[1:2048,1:501]", zero="Zero.fits", flat="Flat2.fits", 
+
 def main():
 	os.chdir(LOCATION)
 	iraf.noao(_doprint=0)
 	iraf.imred(_doprint=0)
 	iraf.ccdred(_doprint=0)
 #	combine_zeros()
-	combine_flat1()
-	combine_flat2()
+#	combine_flat1()
+#	combine_flat2()
+	ccdproc_ngc4725()
 
 main()
