99eac30760f8362d66125a38aba8fcd334d55e95
Stephen <Stephen@enfield173-221.hampshire.edu>
2009-11-03 11:46:25 -0500

    working apsum

--- lib.py
+++ lib.py
@@ -102,11 +102,70 @@ def imcopy(input, output, section, **kwargs):
 def slice_galaxy(name, comp):
 	cood_data = coodproc('input/%s_cood.json' % name)
 	for i in range(len(cood_data)):
-		rotate("%s/base" % name, '%s/r%s' % (name, i),
+		if i < 10:
+			si = "00%s" % i
+		elif i < 100:
+			si = '0%s' % i
+		else:
+			si = '%s' % i
+		rotate("%s/base" % name, '%s/r%s' % (name, si),
 			cood_data[i]['angle'])
-		rotate(comp, '%s/rc%s' % (name, i),
+		rotate(comp, '%s/r%sc' % (name, si),
 			cood_data[i]['angle'])
-		imcopy('%s/r%s' % (name, i), '%s/s%s' % (name, i),
+		imcopy('%s/r%s' % (name, si), '%s/s%s' % (name, si),
 			cood_data[i]['section'])
-		imcopy('%s/rc%s' % (name, i), '%s/sc%s' % (name, i),
+		imcopy('%s/r%sc' % (name, si), '%s/s%sc' % (name, si),
 			cood_data[i]['section'])
+		apsum('%s/s%s' % (name, si), '%s/%s.1d' % (name, si),
+			cood_data[i]['section'])
+		apsum('%s/s%sc' % (name, si), '%s/%sc.1d' % (name, si),
+			cood_data[i]['section'])
+	
+
+def set_aperture(input, section):
+	(row, column) = section[1:-1].split(',')
+	(left, right) = row.split(':')
+	(down, up) = column.split(':')
+	center = (float(up) - float(down)) / 2.
+	rup = center
+	rdown = -center
+	tmp = []
+	tmp.append('begin\taperture %s 1 1024. %s\n' % (input, center))
+	tmp.append('\timage\t%s\n' % input)
+	tmp.append('\taperture\t1\n')
+	tmp.append('\tbeam\t1\n')
+	tmp.append('\tcenter\t1024. %s\n' % center)
+	tmp.append('\tlow\t-1023. %s\n' % rdown)
+	tmp.append('\thigh\t1024. %s\n' % rup)
+	tmp.append('\tbackground\n')
+	tmp.append('\t\txmin -10.\n')
+	tmp.append('\t\txmax 10.\n')
+	tmp.append('\t\tfunction chebyshev\n')
+	tmp.append('\t\torder 1\n')
+	tmp.append('\t\tsample -10:-6,6:10\n')
+	tmp.append('\t\tnaverage -3\n')
+	tmp.append('\t\tniterate 0\n')
+	tmp.append('\t\tlow_reject 3.\n')
+	tmp.append('\t\thigh_reject 3.\n')
+	tmp.append('\t\tgrow 0.\n')
+	tmp.append('\taxis\t2\n')
+	tmp.append('\tcurve\t5\n')
+	tmp.append('\t\t2.\n')
+	tmp.append('\t\t1.\n')
+	tmp.append('\t\t1.\n')
+	tmp.append('\t\t2048.\n')
+	tmp.append('\t\t0.\n')
+	tmp.append('\n')
+	file = open('./database/ap%s' % input.replace('/', '_'), 'w')
+	file.writelines(tmp)
+
+def apsum(input, output, section, **kwargs):
+	load_apextract()
+	set_aperture(input, section)
+	kwargs.setdefault('format', 'onedspec')
+	kwargs.setdefault('interactive', 'no')
+	kwargs.setdefault('find', 'no')
+	kwargs.setdefault('trace', 'no')
+	kwargs.setdefault('fittrace', 'no')
+	iraf.apsum.unlearn()
+	iraf.apsum(input=input, output=output, **kwargs)
