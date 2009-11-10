ce56cdcfbc4132784a07afa7584f6cb688a963b9
Stephen <Stephen@enfield173-221.hampshire.edu>
2009-11-10 11:47:22 -0500

    progress on n3/ngc3169

--- run.py
+++ run.py
@@ -14,12 +14,22 @@ def n3():
 	if not os.path.isdir('./ngc4725'):
 		os.mkdir('./ngc4725')
 	#combine("@lists/ngc4725", "ngc4725/base")
+#after noting down slice begins/ends
 	#rotate_galaxy('ngc4725', 'cghn30030')
 	#imcopy_galaxy('ngc4725')
 	#apsum_galaxy('ngc4725')
-	#reidentify_galaxy('ngc4725', '016.1d.0001')
-	hedit_galaxy('ngc4725')
-	dispcor_galaxy('ngc4725')
+# did a manual identify on 16c, manual reidentify on the other comps
+	#hedit_galaxy('ngc4725')
+	#dispcor_galaxy('ngc4725')
+#ngc3169
+	#ccdproc('@lists/ngc3169', zero="Zero", flat="Flat1")
+	if not os.path.isdir('./ngc3169'):
+		os.mkdir('./ngc3169')
+	#combine('@lists/ngc3169', 'ngc3169/base')
+	# noted slice starts/ends
+	#rotate_galaxy('ngc3169', 'cghn30022')
+	#imcopy_galaxy('ngc3169')
+	#apsum_galaxy('ngc3169')
 
 def n6():
 	location = '../n6'
