e44f4049db52d439e108aa27168840ba75de7559
Stephen <Stephen@enfield173-221.hampshire.edu>
2009-12-12 21:54:21 -0500

    rewrote text_to_json to use type information, also generate the name of output itself

--- text_to_json.py
+++ text_to_json.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-#script for transforming plaintext list of image coordinates into json, for use
-#in geomap things
+#script for transforming plaintext list of image coordinates into json
+# useage: text2json <name.(column1).txt> <name.(column2).txt> <name.types.txt>
 
 import sys, os
 import simplejson as json
 
 def main(args):
 	json_file_name = args.pop()
-	if os.access(json_file_name, os.F_OK):
-		json_file = open(json_file_name, 'r')
-		internal_dict = json.load(json_file)
-		json_file.close()
-	else:
-		internal_dict = {}
-	for file in args:
-		column = raw_input("What column is this for? (%s) " % file)
+	internal_dict = {}
+	type_file = open(args[-1], 'r')
+	types = type_file.readlines()
+	type_file.close()
+	for file in args[:-1]:
+		name, column, ext = file.split('.')
+		#column = raw_input("What column is this for? (%s) " % file)
 		internal_dict.update({column: []})
 		working_file = open(file, 'r')
 		for line in working_file:
@@ -25,9 +24,11 @@ def main(args):
 				tmp = line.split('-')
 				start = tmp[0].strip()
 				end = tmp[1].strip()
+				type = types.pop(0)
 				internal_dict[column].append({"start": start,
-					"end": end})
-	json_file = open(json_file_name, "w")
+					"end": end, 'type': type})
+	json_fn = '%s_coord.json' % name
+	json_file = open(json_fn, "w")
 	json.dump(internal_dict, json_file)
 	json_file.close()
 
