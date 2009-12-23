8b5331239f05d15eaad553b9528d26bbd6f3938c
Stephen <Stephen@saturnine.(none)>
2009-12-22 23:24:07 -0500

    got fixpix working

--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
 import math, os, os.path
 import simplejson as json
 from pyraf import iraf
+import pyfits
 
 def avg(*args):
 	floatNums = [float(x) for x in args]
@@ -49,6 +50,7 @@ def flatcombine(input, **kwargs):
 def ccdproc(images, **kwargs):
 	load_ccdred()
 	kwargs.setdefault('darkcor', 'no')
+	kwargs.setdefault('fixpix', 'no')
 	kwargs.setdefault('biassec', '[2049:2080,1:501]')
 	kwargs.setdefault('trimsec', '[1:2048,1:501]')
 	iraf.ccdproc.unlearn()
@@ -66,8 +68,8 @@ def get_data(name):
 		angles = get_angles(raw_data['coord'])
 		sections, size = get_sections(raw_data['coord'])
 		data = []
-		for i in range(len(angles)):
-			data.append({'angle':angles[i], 'section':sections[i], 
+		for i, angle in enumerate(angles):
+			data.append({'angle':angle, 'section':sections[i], 
 				'size':size[i], 'type':types[i], 'number':i})
 		write_data(name, data)
 	else:
@@ -76,13 +78,13 @@ def get_data(name):
 
 def write_data(name, data):
 	raw_data = get_raw_data(name)
-	raw_data.update['data':data]
+	raw_data.update({'data':data})
 	write_raw_data(name, raw_data)
 
 def write_raw_data(name, raw_data):
 	fn = os.path.join(BASE, 'input/%s.json' % name)
 	data_file = open(fn, 'w')
-	json.dump(data_file, raw_data)
+	json.dump(raw_data, data_file)
 	data_file.close()
 
 def get_raw_data(name):
@@ -105,8 +107,7 @@ def get_value(name, value_name):
 			fn = os.path.combine(BASE, 
 				'%s/%s' % (raw_data['use_sky'], sky_spectra))
 			return fn
-	else:
-		return raw_data[value]
+	return raw_data[value_name]
 	
 
 def get_sections(raw_data):
@@ -115,11 +116,11 @@ def get_sections(raw_data):
 	list2 = raw_data[columns[1]]
 	sections = []
 	size = []
-	for i in range(len(list1)):
-		start1 = list1[i]['start']
-		end1 = list1[i]['end']
-		start2 = list2[i]['start']
-		end2 = list2[i]['end']
+	for item1, item2 in zip(list1, list2):
+		start1 = item1['start']
+		end1 = item1['end']
+		start2 = item2['start']
+		end2 = item2['end']
 		start = int(round(avg(start1, start2)))
 		end = int(round(avg(end1, end2)))
 		sections.append('[1:2048,%s:%s]' % (start, end))
@@ -128,16 +129,15 @@ def get_sections(raw_data):
 
 def get_angles(raw_data):
 	columns = raw_data.keys()
-	print columns
 	list1 = raw_data[columns[0]]
 	list2 = raw_data[columns[1]]
 	run = float(columns[0]) - float(columns[1])
 	angles = []
-	for i in range(len(list1)):		
-		start1 = list1[i]['start']
-		end1 = list1[i]['end']
-		start2 = list2[i]['start']
-		end2 = list2[i]['end']
+	for item1, item2 in zip(list1, list2):		
+		start1 = item1['start']
+		end1 = item1['end']
+		start2 = item2['start']
+		end2 = item2['end']
 		mid1 = avg(start1, end1)
 		mid2 = avg(start2, end2)
 		rise = mid1 - mid2
@@ -150,72 +150,67 @@ def rotate(input, output, angle, **kwargs):
 	iraf.rotate.unlearn()
 	iraf.rotate(input=input, output=output, rotation=-angle, **kwargs)
 
-def imcopy(input, output, section, **kwargs):
+def imcopy(input, output, **kwargs):
 	iraf.imcopy.unlearn()
-	tmp = input + section
-	iraf.imcopy(input=tmp, output=output, **kwargs)
+	iraf.imcopy(input=input, output=output, **kwargs)
 
 def rotate_galaxy(name, comp):
 	data = get_data(name)
 	for i, item in enumerate(data):
-		si = zerocount(i)
-		rotate("%s/base" % name, '%s/r%s' % (name, si), item['angle'])
-		rotate(comp, '%s/r%sc' % (name, si), item['angle'])
+		num = zerocount(i)
+		rotate("%s/base" % name, '%s/r%s' % (name, num), item['angle'])
+		rotate(comp, '%s/r%sc' % (name, num), item['angle'])
 
 def imcopy_galaxy(name):
 	data = get_data(name)
-	os.chdir(name)
-	for i in range(len(data)):
-		si = zerocount(i)
-		imcopy('r%s' % si, 's%s' % si, data[i]['section'])
-		imcopy('r%sc' % si, 's%sc' % si, data[i]['section'])
-	os.chdir('..')
+	for i, item in enumerate(data):
+		num = zerocount(i)
+		imcopy('%s/r%s%s' % (name, num, item['section']), '%s/s%s' % (name, num))
+		imcopy('%s/r%sc%s' % (name, num, item['section']), '%s/s%sc' % (name, num))
 
 def apsum_galaxy(name):
 	data = get_data(name)
-	os.chdir(name)
-	for i in range(len(data)):
-		si = zerocount(i)
-		apsum('s%s' % si, '%s.1d' % si, data[i]['section'])
-		apsum('s%sc' % si, '%sc.1d' % si, data[i]['section'])
-	os.chdir('..')
+	for i, item in enumerate(data):
+		num = zerocount(i)
+		apsum('%s/s%s' % (name, num), '%s/%s.1d' % (name, num), item['section'])
+		apsum('%s/s%sc' % (name, num), '%s/%sc.1d' % (name, num), item['section'])
 
 def reidentify_galaxy(name, reference):
 	data = get_data(name)
-	os.chdir(name)
-	for i in range(len(data)):
-		si = zerocount(i)
-		reidentify(reference, '%sc.1d.0001' % si)
-	os.chdir('..')
+	for i, item in enumerate(data):
+		num = zerocount(i)
+		reidentify(reference, '%s/%sc.1d.0001' % (name, num))
 
 def hedit_galaxy(name):
 	data = get_data(name)
-	os.chdir(name)
-	for i in range(len(data)):
-		si = zerocount(i)
-		hedit('%s.1d.0001' % si, 'REFSPEC1', '%sc.1d.0001' % si)
-	os.chdir('..')
+	for i, item in enumerate(data):
+		num = zerocount(i)
+		hedit('%s/%s.1d.0001' % (name, num), 'REFSPEC1', '%sc.1d.0001' % num)
 
 def dispcor_galaxy(name):
 	data = get_data(name)
 	os.chdir(name)
-	for i in range(len(data)):
-		si = zerocount(i)
-		dispcor('%s.1d.0001' % si, 'd%s.1d' % si)
+	for i, item in enumerate(data):
+		num = zerocount(i)
+		dispcor('%s.1d.0001' % num, 'd%s.1d' % num)
 	os.chdir('..')
 
-def get_scaling(spectra, sky):
+def get_scaling(name, spectra, sky):
 	number = spectra['number']
-	name = 'd%s.1d' % zerocount(number)
+	name = '%s/d%s.1d.fits' % (name, zerocount(number))
+	skyname = '%s.fits' % sky
 	spectrafits = pyfits.open(name)
-	skyfits = pyfits.open(sky)
+	skyfits = pyfits.open(skyname)
 	#skylines: Na D 5893, OI 5578, OI 6301, OI 6365
 	lines = [5893, 5578, 6301, 6365]
 	scalings = []
 	for line in lines:
-		spectra_strength = get_wavelength_strength(spectra, line)
-		sky_strength = get_wavelength_strength(sky, line)
-		scalings.append(spectra_strength / sky_strength)
+		#spectra_strength = get_wavelength_strength(spectrafits, line)
+		#sky_strength = get_wavelength_strength(skyfits, line)
+		spec_peak, spec_cont = get_peak_cont(spectrafits, line, 3)
+		sky_peak, sky_cont = get_peak_cont(skyfits, line, 3)
+		scale = ((spec_peak - spec_cont) / (sky_peak - sky_cont))
+		scalings.append(scale)
 	return avg(*scalings)
 
 def get_wavelength_strength(hdulist, wavelength):
@@ -227,24 +222,49 @@ def get_wavelength_strength(hdulist, wavelength):
 	number = round(tmp / step)
 	return data[number]
 
-def sky_subtract_galaxy(name, sky='',  prefix='', scale=False):
+def get_peak_cont(hdulist, wavelength, search_scope):
+	headers = hdulist[0].header
+	data = hdulist[0].data
+	start = headers['CRVAL1']
+	step = headers['CDELT1']
+	tmp = wavelength - start
+	number = round(tmp / step)
+	search = range(int(number - search_scope), int(number + search_scope))
+	list = [data[i] for i in search]
+	peak = max(list)
+	peak_num = search[list.index(peak)]
+	upcont_num = peak_num
+	while True:
+		if data[upcont_num] >= data[upcont_num + 1]:
+			upcont_num += 1
+		else:
+			break
+	downcont_num = peak_num
+	while True:
+		if data[downcont_num] >= data[downcont_num - 1]:
+			downcont_num -= 1
+		else:
+			break
+	cont = avg(data[upcont_num], data[downcont_num])
+	return peak, cont
+
+def sky_subtract_galaxy(name, sky='', scale=False):
 	data = get_data(name)
-	os.chdir(name)
 	if sky == '':
 		sky = get_value(name, 'sky_spectra')
 	if scale == True:
 		for i, item in enumerate(data):
-			scale = get_scaling(item, sky)
-			si = zerocount(i)
-			scaled_sky = '%ssky.1d.%s' % (prefix, si)
+			scale = get_scaling(name, item, sky)
+			num = zerocount(i)
+			scaled_sky = '%s/sky.1d.%s' % (name, num)
 			sarith(sky, '*', scale, scaled_sky)
-			sarith('d%s.1d' % si, '-', scaled_sky, '%sds%s.1d' % (prefix, si))
+			sarith('%s/d%s.1d' % (name, num), '-', scaled_sky, '%s/ds%s.1d' % 
+				(name, num))
 	else:
-		for i in range(len(data)):
-			si = zerocount(i)
-			sarith('d%s.1d' % si, '-', sky, 
-				'%sds%s.1d' % (prefix, si))
-	os.chdir('..')
+		for i, item in enumerate(data):
+			num = zerocount(i)
+			sarith('%s/d%s.1d' % (name, num), '-', sky, '%sds%s.1d' % 
+				(name, num))
 
 def print_size(name, list=''):
 	data = get_data(name)
@@ -258,7 +278,6 @@ def scale_spectra(input, scale, output):
 
 def combine_sky_spectra(name, out='sky.1d', scale=False, **kwargs):
 	data = get_data(name)
-	os.chdir(name)
 	list = []
 	for i, item in enumerate(data):
 		if item['type'] == 'NIGHTSKY':
@@ -267,17 +286,17 @@ def combine_sky_spectra(name, out='sky.1d', scale=False, **kwargs):
 		flist = []
 		for spectra in list:
 			scale = data[spectra]['size']
-			i = zerocount(spectra)
-			sarith('d%s.1d' % i, '/', scale, 
-				'd%s.1d.scaled' % i)
-			flist.append('d%s.1d.scaled' % i)
+			num = zerocount(spectra)
+			sarith('%s/d%s.1d' % (name, num), '/', scale, 
+				'%s/d%s.1d.scaled' % (name, num))
+			flist.append('%s/d%s.1d.scaled' % (name, num))
 	else:
 		flist = []
 		for spectra in list:
-			flist.append('d%s.1d' % i)
-	scombine(list_convert(flist), out, **kwargs)
-	set_value(name, 'sky_spectra', out)
-	os.chdir('..')
+			num = zerocount(spectra)
+			flist.append('%s/d%s.1d' % (name, num))
+	scombine(list_convert(flist), '%s/%s' % (name, out), **kwargs)
+	set_value(name, 'sky_spectra', '%s/%s' % (name, out))
 
 def list_convert(list):
 	str = list.pop(0)
@@ -289,9 +308,9 @@ def calibrate_galaxy(name, calibration, prefix=''):
 	data = get_data(name)
 	os.chdir(name)
 	sens = os.path.combine(BASE, '%s/%s.sens' % (calibration, calibration))
-	for i in range(len(data)):
-		si = zerocount(i)
-		calibrate('%sds%s.1d' % (prefix, si), sens, '%sdsc%s.1d' % (prefix, si))
+	for i, item in enumerate(data):
+		num = zerocount(i)
+		calibrate('%sds%s.1d' % (prefix, num), sens, '%sdsc%s.1d' % (prefix, num))
 	os.chdir('..')
 
 def zerocount(i):
@@ -301,6 +320,19 @@ def zerocount(i):
 		return '0%s' % i
 	else:
 		return '%s' % i
+
+def fix_image(image, mask):
+	hedit(image, 'BPM', mask)
+	fixpix(image, 'BPM', verbose="yes")
+
+def fix_galaxy(name, mask):
+	imcopy('@lists/%s' % name, '%s/@lists/%s' % (name, name))
+	fix_image('%s/@lists/%s' % (name, name), mask)
+	
+
+def fixpix(image, mask, **kwargs):
+	iraf.fixpix.unlearn()
+	iraf.fixpix(images = image, masks = mask, **kwargs)
 
 def calibrate(input, sens, output, **kwargs):
 	load_kpnoslit()
--- run.py
+++ run.py
@@ -1,28 +1,25 @@
-import sys, os, math, subprocess
+import sys, os, math, subprocess, time
 import simplejson as json
 from pyraf import iraf
 from lib import *
+
+def init_galaxy(name, mask, zero, flat):
+	os.mkdir(name)
+	fix_galaxy(name, mask)
+	ccdproc('%s/@lists/%s' % (name, name), zero=zero, flat=flat)
+	combine('%s/@lists/%s' % (name, name), '%s/base' % name)
 
 def n3_initial():
 	zerocombine("@lists/zero")
 	flatcombine("@lists/flat1", output = "Flat1")
 	flatcombine("@lists/flat2", output = "Flat2")
-#ngc4725
-	ccdproc("@lists/ngc4725", zero="Zero", flat="Flat2", fixfile="Mask")
-	os.mkdir('./ngc4725')
-	combine("@lists/ngc4725", "ngc4725/base")
-#ngc3169
-	ccdproc('@lists/ngc3169', zero="Zero", flat="Flat1", fixfile="Mask")
-	os.mkdir('./ngc3169')
-	combine('@lists/ngc3169', 'ngc3169/base')
-#feige34
-	ccdproc('@lists/feige34', zero="Zero", flat='Flat1', fixfile="Mask")
-	os.mkdir('./feige34')
-	combine('@lists/feige34', 'feige34/base')
-#pg1708+602
-	ccdproc('@lists/pg1708+602', zero='Zero', flat='Flat2', fixfile='Mask')
-	os.mkdir('./pg1708+602')
-	combine('@lists/pg1708+602', 'pg1708+602/base')
+	fix_image('Flat1', 'Mask2.pl')
+	fix_image('Flat2', 'Mask2.pl')
+	fix_image('Zero', 'Mask2.pl')
+	init_galaxy('ngc4725', 'Mask2.pl', 'Zero', 'Flat2')
+	init_galaxy('ngc3169', 'Mask2.pl', 'Zero', 'Flat1')
+	init_galaxy('feige34', 'Mask2.pl', 'Zero', 'Flat1')
+	init_galaxy('pg1708+602', 'Mask2.pl', 'Zero', 'Flat2')
 
 def n3_slices():
 #ngc4725
@@ -46,23 +43,29 @@ def n3_dispersion():
 #ngc3169
 	hedit_galaxy('ngc3169')
 	dispcor_galaxy('ngc3169')
-	combine_sky_spectra('ngc3169', scale=True)
-	sky_subtract_galaxy('ngc3169', scale=True)
 #feige34
 	hedit_galaxy('feige34')
 	dispcor_galaxy('feige34')
-	combine_sky_spectra('feige34', scale=True)
-	sky_subtract_galaxy('feige34', scale=True)
 #pg1708+602
 	hedit_galaxy('pg1708+602')
 	dispcor_galaxy('pg1708+602')
-	combine_sky_spectra('pg1708+602', scale=True)
-	sky_subtract_galaxy('pg1708+602', scale=True)
 #ngc4725
 	hedit_galaxy('ngc4725')
 	dispcor_galaxy('ngc4725')
-	set_value('ngc4725', 'use_sky', 'ngc3169')
-	sky_subtract_galaxy('ngc4725', scale=True)
+
+def n3_skies():
+#ngc3169
+	#combine_sky_spectra('ngc3169', scale=True)
+	sky_subtract_galaxy('ngc3169', scale=True)
+#feige34
+	#combine_sky_spectra('feige34', scale=True)
+	#sky_subtract_galaxy('feige34', scale=True)
+#pg1708+602
+	#combine_sky_spectra('pg1708+602', scale=True)
+	#sky_subtract_galaxy('pg1708+602', scale=True)
+#ngc4725
+	#set_value('ngc4725', 'use_sky', 'ngc3169')
+	#sky_subtract_galaxy('ngc4725', scale=True)
 
 def n3_calibrate():
 	calibrate_galaxy('ngc3169', 'feige34')
@@ -74,9 +77,10 @@ def n3():
 	set_BASE(os.getcwd())
 	n3_initial()
 	#then make sure that you've got strip coordinate files
-	n3_slices()
+	#n3_slices()
 	#then identify everything
 	#n3_dispersion()
+	#n3_skies()
 	#then run standard and sensfunc manually
 	#n3_calibrate()
 	
