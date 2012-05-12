036177af4d2cdc98ffc4ff56e597223ecf592b5e
tungolcraft <tungolcraeft@gmail.com>
2012-05-12 08:46:12 -0400

    get rid of hard tabs

--- analyze.py
+++ analyze.py
@@ -1074,51 +1074,51 @@ def make_comparison_table(galaxies, other):
 
 
 def main():
-	os.chdir('../n3')
-	ngc3169 = GalaxyClass('ngc3169')
-	ngc3169.name = 'NGC 3169'
-	ngc3169.redshift = 0.004130
-	ngc3169.center = '10:14:15.0 +03:27:58'
-	ngc3169.distance = 20100 #kpc
-	ngc3169.r25 = 11.57 #kpc
-	ngc3169.type = 'Sa'
-	ngc3169.bar = 'A'
-	ngc3169.ring = 's'
-	ngc3169.env = 'pair'
-	
-	ngc4725 = GalaxyClass('ngc4725')
-	ngc4725.name = 'NGC 4725'
-	ngc4725.redshift = 0.004023
-	ngc4725.center = '12:50:26.6 +25:30:03'
-	ngc4725.distance = 13918 #kpc
-	ngc4725.r25 = 26.23 #kpc
-	ngc4725.type = 'Sab'
-	ngc4725.bar = 'AB'
-	ngc4725.ring = 'r'
-	ngc4725.env = 'pair'
-	
-	galaxies = [ngc3169, ngc4725]
-	other_data = get_other()
-	
-	for galaxy in galaxies:
-		galaxy.run()
-		galaxy.fit_OH()
-		galaxy.output_tables()
-		galaxy.output_graphs()
-	
-	for galaxy in other_data:
-		galaxy.fit_OH()
-			
-	compare_basic(galaxies, other_data)
-	compare_type(galaxies, other_data)
-	compare_bar(galaxies, other_data)
-	compare_ring(galaxies, other_data)
-	compare_env(galaxies, other_data)
-	make_comparison_table(galaxies, other_data)
-	compare_type_table(galaxies, other_data)
-	compare_bar_table(galaxies, other_data)
-	compare_ring_table(galaxies, other_data)
-	compare_env_table(galaxies, other_data)
-	
+    os.chdir('../n3')
+    ngc3169 = GalaxyClass('ngc3169')
+    ngc3169.name = 'NGC 3169'
+    ngc3169.redshift = 0.004130
+    ngc3169.center = '10:14:15.0 +03:27:58'
+    ngc3169.distance = 20100 #kpc
+    ngc3169.r25 = 11.57 #kpc
+    ngc3169.type = 'Sa'
+    ngc3169.bar = 'A'
+    ngc3169.ring = 's'
+    ngc3169.env = 'pair'
+    
+    ngc4725 = GalaxyClass('ngc4725')
+    ngc4725.name = 'NGC 4725'
+    ngc4725.redshift = 0.004023
+    ngc4725.center = '12:50:26.6 +25:30:03'
+    ngc4725.distance = 13918 #kpc
+    ngc4725.r25 = 26.23 #kpc
+    ngc4725.type = 'Sab'
+    ngc4725.bar = 'AB'
+    ngc4725.ring = 'r'
+    ngc4725.env = 'pair'
+    
+    galaxies = [ngc3169, ngc4725]
+    other_data = get_other()
+    
+    for galaxy in galaxies:
+        galaxy.run()
+        galaxy.fit_OH()
+        galaxy.output_tables()
+        galaxy.output_graphs()
+    
+    for galaxy in other_data:
+        galaxy.fit_OH()
+            
+    compare_basic(galaxies, other_data)
+    compare_type(galaxies, other_data)
+    compare_bar(galaxies, other_data)
+    compare_ring(galaxies, other_data)
+    compare_env(galaxies, other_data)
+    make_comparison_table(galaxies, other_data)
+    compare_type_table(galaxies, other_data)
+    compare_bar_table(galaxies, other_data)
+    compare_ring_table(galaxies, other_data)
+    compare_env_table(galaxies, other_data)
+    
 
 main()
