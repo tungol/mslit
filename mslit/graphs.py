92997bf2f0775b47eb55ebb2b6da7831b8a1fa63
tungolcraft <tungolcraeft@gmail.com>
2012-05-21 05:08:58 -0400

    refactoring graphs.py, lots of things broke

--- mslit/graphs.py
+++ mslit/graphs.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+graphs.py - functions for oupting graphs
+
+Single galaxy graphs: graph_metallicity, graph_sfr, graph_sfr_metals
+Mutiple galaxy graphs: compare, compare_basic
+"""
+
 import matplotlib
 from matplotlib.backends.backend_ps import FigureCanvasPS as FigureCanvas
 import numpy
@@ -8,48 +18,66 @@ matplotlib.rc('text', usetex=True)
 matplotlib.rc('font', family='serif', serif='Computer Modern Roman')
 
 
-## Make some graphs ##
+## Generic plotting functions ##
+
+
+def add_solar_metallicity(axes):
+    xbound = axes.get_xbound()
+    t = numpy.arange(0, xbound[1] * (4 / 3.), xbound[1] * 0.05)
+    #overplot solar metalicity
+    solardata = 8.69 + t * 0
+    axes.plot(t, solardata, 'k--')
+    axes.text(xbound[1] * (301 / 300.), 8.665, r'$Z_\odot$',
+              transform=axes.transData)
+
+
+def plot(galaxy_sets, axes, colors, xkey, ykey, only_corrected=False):
+    for group, color in zip(galaxy_sets, colors):
+        for galaxy in group:
+            spectra = galaxy.spectra
+            if only_corrected:
+                spectra = [s for s in spectra if s.corrected]
+            x = [s.__dict__[xkey] for s in spectra]
+            y = [s.__dict__[ykey] for s in spectra]
+            remove_nan(x, y)
+            x = numpy.array(x)
+            y = numpy.array(y)
+            if xkey is 'rdistance':
+                x = x / galaxy.r25
+            axes.plot(x, y, color)
+
+
+## Single Galaxy Plots ##
 
 
 def graph_metalicity(galaxy):
-    spectra = galaxy.spectra
-    OH = [s.OH for s in spectra]
-    r = [s.rdistance for s in spectra]
-    remove_nan(OH, r)
-    OH = numpy.array(OH)
-    r = numpy.array(r)
-    r = r / galaxy.r25
+    """Graph the O/H metallicity of a galaxy versus R/R_25 distance. Include
+       a linear fitted function."""
+    # setup the graph
     fig = matplotlib.figure.Figure(figsize=(5, 5))
     canvas = FigureCanvas(fig)
-    #plot the data
     axes = fig.add_axes((.125, .1, .775, .8))
     axes.set_xlabel(r'$R/R_{25}$')
     axes.set_ylabel(r'$12 + \log{\textnormal{O/H}}$')
     axes.set_autoscale_on(False)
     axes.set_xbound(lower=0, upper=1.5)
     axes.set_ybound(lower=8.0, upper=9.7)
-    axes.plot(r, OH, 'co')
-    #overplot the fitted function
+    # plot the data
+    plot(((galaxy,),), axes, ('co',), 'rdistance', 'OH')
+    # overplot the fitted function
     t = numpy.arange(0, 2, .1)
     fit = galaxy.fit[0]
     fitdata = fit[1] + t * fit[0]
     axes.plot(t, fitdata, 'k-')
     #overplot solar metalicity
-    solardata = 8.69 + t * 0
-    axes.plot(t, solardata, 'k--')
-    # label solar metalicity
-    axes.text(1.505, 8.665, r'$Z_\odot$', transform=axes.transData)
+    add_solar_metallicity(axes)
     canvas.print_eps('tables/%s_metals.eps' % galaxy.num)
 
 
 def graph_sfr(galaxy):
-    spectra = [s for s in galaxy.spectra if s.corrected]
-    SFR = [s.SFR for s in spectra]
-    r = [s.rdistance for s in spectra]
-    remove_nan(SFR, r)
-    SFR = numpy.array(SFR)
-    r = numpy.array(r)
-    r = r / galaxy.r25
+    """Graph the star formation rate within a galaxy versus the R/R_25
+       distance."""
+    # set up the graph
     fig = matplotlib.figure.Figure(figsize=(5, 5))
     canvas = FigureCanvas(fig)
     axes = fig.add_axes((.125, .1, .775, .8))
@@ -57,92 +85,32 @@ def graph_sfr(galaxy):
     axes.set_ylabel(r'SFR(M$_\odot$ / year)')
     axes.set_autoscalex_on(False)
     axes.set_xbound(lower=0, upper=1.5)
-    #plot the data
-    axes.plot(r, SFR, 'co')
+    # plot the data
+    plot(((galaxy,),), axes, ('co',), 'rdistance', 'SFR')
     canvas.print_eps('tables/%s_sfr.eps' % galaxy.num)
 
 
 def graph_sfr_metals(galaxy):
+    """Graph metallicity versus star formation rate, excluding regions which
+       couldn't be extinction corrected."""
     fig = matplotlib.figure.Figure(figsize=(5, 5))
     canvas = FigureCanvas(fig)
     axes = fig.add_axes((.125, .1, .775, .8))
     axes.set_xlabel(r'SFR(M$_\odot$ / year)')
     axes.set_ylabel(r'$12 + \log{\textnormal{O/H}}$')
-    spectra = galaxy.spectra
-    #remove uncorrected values
-    for spectrum in spectra[:]:
-        if spectrum.num[-1] == '*':
-            spectra.remove(spectrum)
-    OH = [s.OH for s in spectra]
-    SFR = [s.SFR for s in spectra]
-    remove_nan(OH, SFR)
-    axes.plot(SFR, OH, 'co')
+    plot(((galaxy,),), axes, ('co',), 'SFR', 'OH', only_corrected=True)
     axes.set_xbound(lower=0)
     axes.set_ybound(lower=8.0, upper=9.7)
     axes.set_autoscale_on(False)
-    xbound = axes.get_xbound()
-    t = numpy.arange(0, xbound[1] * 1.5, xbound[1] * 0.05)
-    #overplot solar metalicity
-    solardata = 8.69 + t * 0
-    axes.plot(t, solardata, 'k--')
-    axes.text(xbound[1] * 1.01, 8.665, r'$Z_\odot$')
+    add_solar_metallicity(axes)
     canvas.print_eps('tables/%s_sfr-metal.eps' % galaxy.num)
 
 
-def compare_basic(galaxies, other):
-    fig = matplotlib.figure.Figure(figsize=(5, 5))
-    canvas = FigureCanvas(fig)
-    axes = fig.add_axes((.125, .1, .775, .8))
-    axes.set_xlabel(r'$R/R_{25}$')
-    axes.set_ylabel(r'$12 + \log{\textnormal{O/H}}$')
-    #overplot solar metalicity
-    t = numpy.arange(0, 2, .1)
-    solardata = 8.69 + t * 0
-    axes.plot(t, solardata, 'k--')
-    axes.text(1.505, 8.665, r'$Z_\odot$', transform=axes.transData)
-    # plot the other data as black diamonds
-    for galaxy in other:
-        spectra = galaxy.spectra
-        OH = [s.OH for s in spectra]
-        r = [s.rdistance for s in spectra]
-        remove_nan(OH, r)
-        OH = numpy.array(OH)
-        r = numpy.array(r)
-        r = r / galaxy.r25
-        axes.plot(r, OH, 'wd')
-    #plot my galaxies
-    data = []
-    for galaxy in galaxies:
-        spectra = galaxy.spectra
-        OH = [s.OH for s in spectra]
-        r = [s.rdistance for s in spectra]
-        remove_nan(OH, r)
-        OH = numpy.array(OH)
-        r = numpy.array(r)
-        r = r / galaxy.r25
-        data.append((r, OH))
-    axes.plot(data[0][0], data[0][1], 'r^')
-    axes.plot(data[1][0], data[1][1], 'co')
-    axes.set_xbound(lower=0, upper=1.5)
-    axes.set_ybound(lower=8.0, upper=9.7)
-    canvas.print_eps('tables/basic_comparison.eps')
+## Mutliple Galaxy Plots ##
 
-
-def plot(galaxies, key, axes, groups):
-    colors = ['y^', 'rd', 'mp', 'bD']
-    for galaxy in galaxies:
-        spectra = galaxy.spectra
-        OH = [s.OH for s in spectra]
-        r = [s.rdistance for s in spectra]
-        remove_nan(OH, r)
-        OH = numpy.array(OH)
-        r = numpy.array(r)
-        r = r / galaxy.r25
-        for group, color in zip(groups, colors):
-            if galaxy.__dict__[key] in group:
-                axes.plot(r, OH, color)
 
 def compare(galaxies, other, groups, key):
+    colors = ['y^', 'rd', 'mp', 'bD']
     fig = matplotlib.figure.Figure(figsize=(5, 5))
     canvas = FigureCanvas(fig)
     axes = fig.add_axes((.125, .1, .775, .8))
@@ -153,10 +121,36 @@ def compare(galaxies, other, groups, key):
     solardata = 8.69 + t * 0
     axes.plot(t, solardata, 'k--')
     axes.text(1.505, 8.665, r'$Z_\odot$')
-    # plot the other data
-    plot(other, key, axes, groups.values())
-    #plot my galaxies
-    plot(galaxies, key, axes, groups.values())
+    # plot the data
+    galaxies += other
+    galaxy_sets = []
+    for group in groups:
+        galaxy_set = []
+        for galaxy in galaxies:
+            if galaxy.__dict__[key] in group:
+                galaxy_set.append(galaxy)
+        galaxy_sets.append(galaxy_set)
+    plot(galaxy_sets, axes, colors, 'rdistance', 'OH')
     axes.set_xbound(lower=0, upper=1.5)
     axes.set_ybound(lower=8.0, upper=9.7)
     canvas.print_eps('tables/%s_comparison.eps' % key)
+
+
+def compare_basic(galaxies, other):
+    """Print metallicity versus """
+    fig = matplotlib.figure.Figure(figsize=(5, 5))
+    canvas = FigureCanvas(fig)
+    axes = fig.add_axes((.125, .1, .775, .8))
+    axes.set_xlabel(r'$R/R_{25}$')
+    axes.set_ylabel(r'$12 + \log{\textnormal{O/H}}$')
+    #overplot solar metalicity
+    t = numpy.arange(0, 2, .1)
+    solardata = 8.69 + t * 0
+    axes.plot(t, solardata, 'k--')
+    axes.text(1.505, 8.665, r'$Z_\odot$', transform=axes.transData)
+    # mine in color, the other data as black diamonds
+    plot((other, (galaxies[0],), (galaxies[1],)), axes, ('wd', 'r^', 'co'),
+         'rdistance', 'OH')
+    axes.set_xbound(lower=0, upper=1.5)
+    axes.set_ybound(lower=8.0, upper=9.7)
+    canvas.print_eps('tables/basic_comparison.eps')
